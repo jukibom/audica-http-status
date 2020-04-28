89516820ae17d5184f31ed349dddab8dbbbef917
Jay Faulkner <jayleefaulkner@gmail.com>
2020-04-28 21:56:44 +0100

    Initial (actual) commit

---
+++ AudicaHTTPStatus/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Collections;
+using System.Threading;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using NET_SDK;
+using NET_SDK.Harmony;
+using NET_SDK.Reflection;
+using System.Runtime.InteropServices;
+using UnityEngine.PostProcessing;
+using WebSocketSharp;
+
+namespace AudicaHTTPStatus
+{
+	public static class BuildInfo
+	{
+		public const string Name = "AudicaHTTPStatus"; // Name of the Mod.  (MUST BE SET)
+		public const string Author = "jukibom"; // Author of the Mod.  (Set as null if none)
+		public const string Company = null; // Company that made the Mod.  (Set as null if none)
+		public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+		public const string DownloadLink = "https://github.com/jukibom/audica-http-status"; // Download Link for the Mod.  (Set as null if none)
+	}
+
+	public class AudicaHTTPStatus : MelonMod
+	{
+		
+		public override void OnApplicationStart()
+		{
+			Instance instance = Manager.CreateInstance("TimingAssist");
+		}
+	}
+}
