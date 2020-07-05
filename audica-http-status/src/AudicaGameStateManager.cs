eaac65b1ce4fc64df912a510650d37995ae89221
Jay Faulkner <jayleefaulkner@gmail.com>
2020-07-05 20:03:39 +0100

    feat: moved over to http-starter project layout +latest melonloader

---
+++ audica-http-status/src/AudicaGameStateManager.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudicaHTTPStatus.util;
+using UnityEngine;
+
+namespace AudicaHTTPStatus {
+
+	class AudicaGameStateManager {
+
+		// Audica game classes
+		public static ScoreKeeper scoreKeeper;
+        public static GameplayModifiers modifiers;
+        public static PlayerPreferences prefs;
+        public static KataConfig config;
+        public static SongCues songCues;
+
+		// State containers
+		private AudicaGameState gameState;
+        private AudicaSongState songState;
+
+        // Util
+        private SongLengthCalculator songCalculator;
+
+        private bool songPlaying = false;
+        public SongList.SongData songData { get; set; } 
+
+        public AudicaGameStateManager() {
+            this.initialiseStateManagers();
+            this.clearGameState();
+            this.clearSongState();
+        }
+
+		public AudicaGameState GameState {
+			get {
+				return this.gameState;
+			}
+		}
+
+		public AudicaSongState SongState {
+			get {
+				return this.songState;
+			}
+		}
+
+        // Called every tick, don't do anything too heavy in here!
+		public void Update() {
+            this.pollGameState();
+            this.pollSongState();
+		}
+
+		public void SongStart(SongList.SongData song) {
+            this.initialiseStateManagers();
+            this.songCalculator = new SongLengthCalculator();
+            this.songData = song;
+            this.songPlaying = true;
+        }
+
+        public void SongRestart() {
+            this.initialiseStateManagers();
+            this.clearSongState();
+		}
+
+		public void SongEnd() {
+            this.songPlaying = false;
+            this.clearSongState();
+        }
+
+        private void initialiseStateManagers() {
+            AudicaGameStateManager.scoreKeeper = UnityEngine.Object.FindObjectOfType<ScoreKeeper>();
+            AudicaGameStateManager.modifiers = UnityEngine.Object.FindObjectOfType<GameplayModifiers>();
+            AudicaGameStateManager.prefs = UnityEngine.Object.FindObjectOfType<PlayerPreferences>();
+            AudicaGameStateManager.config = UnityEngine.Object.FindObjectOfType<KataConfig>();
+            AudicaGameStateManager.songCues = UnityEngine.Object.FindObjectOfType<SongCues>();
+        }
+
+		private void clearGameState() {
+			this.gameState = new AudicaGameState();
+        }
+
+        private void clearSongState() {
+            this.songState = new AudicaSongState();
+
+            // this is potentially read all the time but only updates if a song is playing so we should initialise everything to actual sane values ...
+            this.songState.songId = "";
+            this.songState.songName = "";
+            this.songState.songArtist = "";
+            this.songState.songAuthor = "";
+            this.songState.difficulty = "";
+            this.songState.classification = "";
+            this.songState.songLength = TimeSpan.FromSeconds(0).ToString();
+            this.songState.timeElapsed = TimeSpan.FromSeconds(0).ToString();
+            this.songState.timeRemaining = TimeSpan.FromSeconds(0).ToString();
+            this.songState.progress = 0;
+            this.songState.currentTick = 0;
+            this.songState.ticksTotal = 0;
+            this.songState.songSpeed = 1;
+            this.songState.health = 1;
+            this.songState.score = 0;
+            this.songState.scoreMultiplier = 1;
+            this.songState.streak = 0;
+            this.songState.highScore = 0;
+            this.songState.isNoFailMode = AudicaGameStateManager.prefs ? AudicaGameStateManager.prefs.NoFail.mVal : false;
+            this.songState.isPracticeMode = AudicaGameStateManager.config ? AudicaGameStateManager.config.practiceMode : false;
+            this.songState.isFullComboSoFar = true;
+            this.songState.modifiers = AudicaGameStateManager.modifiers ? AudicaGameStateManager.modifiers.GetCurrentModifiers()
+                .Select((GameplayModifiers.Modifier mod) => GameplayModifiers.GetModifierString(mod))
+                .ToList<string>()
+                : new List<string>();
+        }
+
+        private void pollGameState() {
+            // prefs (here's hoping color utility isn't dog slow!)
+            this.gameState.leftColor = AudicaGameStateManager.prefs ? ColorUtility.ToHtmlStringRGB(AudicaGameStateManager.prefs.GunColorLeft.mVal) : "#000000";
+            this.gameState.rightColor = AudicaGameStateManager.prefs ? ColorUtility.ToHtmlStringRGB(AudicaGameStateManager.prefs.GunColorRight.mVal) : "#000000";
+            this.gameState.targetSpeed = AudicaGameStateManager.prefs ? AudicaGameStateManager.prefs.TargetSpeedMultiplier.mVal : 1;
+            this.gameState.meleeSpeed = AudicaGameStateManager.prefs ? AudicaGameStateManager.prefs.MeleeSpeedMultiplier.mVal : 1;
+            this.gameState.aimAssist = AudicaGameStateManager.prefs ? AudicaGameStateManager.prefs.AimAssistAmount.mVal : 1;
+        }
+
+        private void pollSongState() {
+            if (this.songPlaying) {
+                string songClass = "custom";
+                if (this.songData.IsCoreSong()) {
+                    songClass = "ost";
+                }
+                if (this.songData.dlc) {
+                    songClass = "dlc";
+                }
+                if (this.songData.extrasSong) {
+                    songClass = "extras";
+                }
+
+                // We don't want to calculate the ticks to the end of the song, it keeps playing!
+                // Instead get the last target (plus its length) as the end ticks
+                UnhollowerBaseLib.Il2CppReferenceArray<SongCues.Cue> cues = AudicaGameStateManager.songCues.mCues.cues;
+                SongCues.Cue endCue = cues[cues.Length - 1];
+                float songEndTicks = endCue.tick + endCue.tickLength;
+
+                float currentTick = AudicaGameStateManager.scoreKeeper.mLastTick;
+
+                float totalTimeMs = this.songCalculator.SongLengthMilliseconds;
+                float currentTimeMs = this.songCalculator.GetSongPositionMilliseconds(currentTick);
+                float remainingTimeMs = totalTimeMs - currentTimeMs;
+                
+                this.songState.songId = this.songData.songID;
+                this.songState.songName = this.songData.title;
+                this.songState.songArtist = this.songData.artist;
+                this.songState.songAuthor = this.songData.author;
+                this.songState.difficulty = KataConfig.GetDifficultyName(AudicaGameStateManager.config.GetDifficulty());
+                this.songState.classification = songClass;
+                this.songState.songLength = TimeSpan.FromMilliseconds(Convert.ToInt64(totalTimeMs)).ToString();
+                this.songState.timeElapsed = TimeSpan.FromMilliseconds(Convert.ToInt64(currentTimeMs)).ToString();
+                this.songState.timeRemaining = TimeSpan.FromMilliseconds(Convert.ToInt64(remainingTimeMs)).ToString();
+                this.songState.progress = currentTimeMs / totalTimeMs;
+                this.songState.currentTick = currentTick;
+                this.songState.ticksTotal = songEndTicks;
+                this.songState.songSpeed = KataConfig.GetCueDartSpeedMultiplier();      // TODO: not a clue what this value actually is but it's not the speed multiplier!
+                this.songState.health = AudicaGameStateManager.scoreKeeper.GetHealth();
+                this.songState.score = AudicaGameStateManager.scoreKeeper.mScore;
+                this.songState.scoreMultiplier = AudicaGameStateManager.scoreKeeper.GetRawMultiplier();
+                this.songState.streak = AudicaGameStateManager.scoreKeeper.GetStreak();
+                this.songState.highScore = AudicaGameStateManager.scoreKeeper.GetHighScore();
+                this.songState.isNoFailMode = AudicaGameStateManager.prefs.NoFail.mVal;
+                this.songState.isPracticeMode = AudicaGameStateManager.config.practiceMode;
+                this.songState.isFullComboSoFar = AudicaGameStateManager.scoreKeeper.GetIsFullComboSoFar();
+                this.songState.modifiers = AudicaGameStateManager.modifiers.GetCurrentModifiers()
+                    .Select((GameplayModifiers.Modifier mod) => GameplayModifiers.GetModifierString(mod))
+                    .ToList<string>();
+            }
+        }
+    }
+}
---
+++ audica-http-status/src/Main.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Collections;
+using System.Threading;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using System.Runtime.InteropServices;
+using UnityEngine.PostProcessing;
+
+namespace AudicaHTTPStatus
+{
+	public static class BuildInfo {
+		public const string Name = "AudicaHTTPStatus"; // Name of the Mod.  (MUST BE SET)
+		public const string Author = "jukibom"; // Author of the Mod.  (Set as null if none)
+		public const string Company = null; // Company that made the Mod.  (Set as null if none)
+		public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+		public const string DownloadLink = "https://github.com/jukibom/audica-http-status"; // Download Link for the Mod.  (Set as null if none)
+	}
+
+	public class AudicaHTTPStatus : MelonMod {
+
+		private static AudicaGameStateManager audicaGameState;
+		private static AudicaTargetStateManager targetState;
+        private static SongList.SongData selectedSongData;
+		private Encoder encoder;
+		private HTTPServer httpServer;
+
+        public static Patch selectSong;
+		public static Patch playSong;
+		public static Patch restartSong;
+		public static Patch endSong;
+		public static Patch targetHit;
+		public static Patch targetMiss;
+		public static Patch targetMissAim;
+		public static Patch targetMissEarlyLate;
+		public static Patch misfire;
+
+		public override void OnApplicationStart() {
+			Instance instance = Manager.CreateInstance("AudicaHTTPStatus");
+
+            // song selection + play state
+            AudicaHTTPStatus.selectSong = instance.Patch(SDK.GetClass("SongSelectItem").GetMethod("OnSelect"), typeof(AudicaHTTPStatus).GetMethod("SelectSong"));
+			AudicaHTTPStatus.playSong = instance.Patch(SDK.GetClass("ScoreKeeper").GetMethod("Start"), typeof(AudicaHTTPStatus).GetMethod("StartSong"));
+			AudicaHTTPStatus.restartSong = instance.Patch(SDK.GetClass("InGameUI").GetMethod("Restart"), typeof(AudicaHTTPStatus).GetMethod("RestartSong"));
+			AudicaHTTPStatus.endSong = instance.Patch(SDK.GetClass("InGameUI").GetMethod("ReturnToSongList"), typeof(AudicaHTTPStatus).GetMethod("EndSong"));
+
+            // target event handling
+			AudicaHTTPStatus.targetHit = instance.Patch(SDK.GetClass("GameplayStats").GetMethod("ReportTargetHit"), typeof(AudicaHTTPStatus).GetMethod("TargetHit"));
+			AudicaHTTPStatus.targetMiss = instance.Patch(SDK.GetClass("GameplayStats").GetMethod("ReportShotNothing"), typeof(AudicaHTTPStatus).GetMethod("TargetMiss"));
+			AudicaHTTPStatus.targetMissAim = instance.Patch(SDK.GetClass("GameplayStats").GetMethod("ReportTargetAimMiss"), typeof(AudicaHTTPStatus).GetMethod("TargetMissAim"));
+			AudicaHTTPStatus.targetMissEarlyLate = instance.Patch(SDK.GetClass("GameplayStats").GetMethod("ReportTargetEarlyLate"), typeof(AudicaHTTPStatus).GetMethod("TargetMissEarlyLate"));
+			AudicaHTTPStatus.misfire = instance.Patch(SDK.GetClass("GameplayStats").GetMethod("ReportMisfire"), typeof(AudicaHTTPStatus).GetMethod("Misfire"));
+
+			AudicaHTTPStatus.audicaGameState = new AudicaGameStateManager();
+			AudicaHTTPStatus.targetState = new AudicaTargetStateManager();
+
+
+            this.encoder = new Encoder();
+
+			this.httpServer = new HTTPServer(() => {
+				return this.encoder.Status(
+					AudicaHTTPStatus.audicaGameState.GameState,
+					AudicaHTTPStatus.audicaGameState.SongState
+				);
+			});
+			this.httpServer.Initialise();
+		}
+
+		public override void OnApplicationQuit() {
+			this.httpServer.Shutdown();
+		}
+
+		public override void OnUpdate() {
+			AudicaHTTPStatus.audicaGameState.Update();
+		}
+
+        public static void SelectSong(IntPtr @this) {
+            AudicaHTTPStatus.selectSong.InvokeOriginal(@this);
+            SongSelectItem button = new SongSelectItem(@this);
+            AudicaHTTPStatus.selectedSongData = button.mSongData;
+        }
+
+		public static void StartSong(IntPtr @this) {
+			AudicaHTTPStatus.playSong.InvokeOriginal(@this);
+			AudicaHTTPStatus.audicaGameState.SongStart(AudicaHTTPStatus.selectedSongData);
+		}
+
+		public static void RestartSong(IntPtr @this) {
+			AudicaHTTPStatus.restartSong.InvokeOriginal(@this);
+			AudicaHTTPStatus.audicaGameState.SongRestart();
+		}
+
+		public static void EndSong(IntPtr @this) {
+			AudicaHTTPStatus.endSong.InvokeOriginal(@this);
+			AudicaHTTPStatus.audicaGameState.SongEnd();
+		}
+        
+		/** TARGET EVENT HANDLING **/
+		public unsafe static void TargetHit(IntPtr @this, IntPtr cue, float tick, Vector2 targetHitPos) {
+			AudicaHTTPStatus.targetHit.InvokeOriginal(@this, new IntPtr[] {
+				cue,
+				new IntPtr((void*)(&tick)),
+				new IntPtr((void*)(&targetHitPos))
+			});
+
+			AudicaTargetHitState targetHit = AudicaHTTPStatus.targetState.TargetHit(targetHitPos);
+			// TODO: feed output into JSON parser then to HTTP server as websocket event
+		}
+
+		public unsafe static void TargetMiss(IntPtr @this) {
+			AudicaHTTPStatus.targetMiss.InvokeOriginal(@this);
+
+			AudicaTargetFailState targetMiss = AudicaHTTPStatus.targetState.TargetMiss();
+		}
+
+		public unsafe static void TargetMissAim(IntPtr @this, IntPtr cue, Vector2 targetMissPos) {
+			AudicaHTTPStatus.targetMissAim.InvokeOriginal(@this, new IntPtr[] {
+				cue,
+				new IntPtr((void*)(&targetMissPos))
+			});
+
+			AudicaTargetFailState targetMiss = AudicaHTTPStatus.targetState.TargetMissAim();
+		}
+
+		public unsafe static void TargetMissEarlyLate(IntPtr @this, IntPtr cue, float tick) {
+			AudicaHTTPStatus.targetMissEarlyLate.InvokeOriginal(@this, new IntPtr[] {
+				cue,
+				new IntPtr((void*)(&tick)),
+			});
+
+			AudicaTargetFailState targetMiss = AudicaHTTPStatus.targetState.TargetMissEarlyLate(tick);
+		}
+
+		public unsafe static void Misfire(IntPtr @this) {
+			AudicaHTTPStatus.misfire.InvokeOriginal(@this);
+
+			// TODO (event with hand that misfired?)
+		}
+	}
+}
