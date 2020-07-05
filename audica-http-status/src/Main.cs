70b7b89b385a95bd123550f09dcc58e28aef8cec
Jay Faulkner <jayleefaulkner@gmail.com>
2020-07-05 21:13:15 +0100

    feat: convert basic game state hooks to harmony format

--- audica-http-status/src/Main.cs
+++ audica-http-status/src/Main.cs
@@ -10,6 +10,9 @@ using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using System.Runtime.InteropServices;
 using UnityEngine.PostProcessing;
+using Harmony;
+using UnhollowerBaseLib;
+using System.Reflection;
 
 namespace AudicaHTTPStatus
 {
@@ -23,48 +26,26 @@ namespace AudicaHTTPStatus
 
 	public class AudicaHTTPStatus : MelonMod {
 
-		private static AudicaGameStateManager audicaGameState;
-		private static AudicaTargetStateManager targetState;
-        private static SongList.SongData selectedSongData;
+        internal static AudicaGameStateManager AudicaGameState { get; set; }
+        internal static AudicaTargetStateManager TargetState { get; set; }
+
+        public static SongList.SongData selectedSongData;
 		private Encoder encoder;
 		private HTTPServer httpServer;
 
-        public static Patch selectSong;
-		public static Patch playSong;
-		public static Patch restartSong;
-		public static Patch endSong;
-		public static Patch targetHit;
-		public static Patch targetMiss;
-		public static Patch targetMissAim;
-		public static Patch targetMissEarlyLate;
-		public static Patch misfire;
+        public override void OnApplicationStart() {
+            var instance = HarmonyInstance.Create("AudicaHTTPStatus");
+            Hooks.ApplyHooks(instance);
 
-		public override void OnApplicationStart() {
-			Instance instance = Manager.CreateInstance("AudicaHTTPStatus");
-
-            // song selection + play state
-            AudicaHTTPStatus.selectSong = instance.Patch(SDK.GetClass("SongSelectItem").GetMethod("OnSelect"), typeof(AudicaHTTPStatus).GetMethod("SelectSong"));
-			AudicaHTTPStatus.playSong = instance.Patch(SDK.GetClass("ScoreKeeper").GetMethod("Start"), typeof(AudicaHTTPStatus).GetMethod("StartSong"));
-			AudicaHTTPStatus.restartSong = instance.Patch(SDK.GetClass("InGameUI").GetMethod("Restart"), typeof(AudicaHTTPStatus).GetMethod("RestartSong"));
-			AudicaHTTPStatus.endSong = instance.Patch(SDK.GetClass("InGameUI").GetMethod("ReturnToSongList"), typeof(AudicaHTTPStatus).GetMethod("EndSong"));
-
-            // target event handling
-			AudicaHTTPStatus.targetHit = instance.Patch(SDK.GetClass("GameplayStats").GetMethod("ReportTargetHit"), typeof(AudicaHTTPStatus).GetMethod("TargetHit"));
-			AudicaHTTPStatus.targetMiss = instance.Patch(SDK.GetClass("GameplayStats").GetMethod("ReportShotNothing"), typeof(AudicaHTTPStatus).GetMethod("TargetMiss"));
-			AudicaHTTPStatus.targetMissAim = instance.Patch(SDK.GetClass("GameplayStats").GetMethod("ReportTargetAimMiss"), typeof(AudicaHTTPStatus).GetMethod("TargetMissAim"));
-			AudicaHTTPStatus.targetMissEarlyLate = instance.Patch(SDK.GetClass("GameplayStats").GetMethod("ReportTargetEarlyLate"), typeof(AudicaHTTPStatus).GetMethod("TargetMissEarlyLate"));
-			AudicaHTTPStatus.misfire = instance.Patch(SDK.GetClass("GameplayStats").GetMethod("ReportMisfire"), typeof(AudicaHTTPStatus).GetMethod("Misfire"));
-
-			AudicaHTTPStatus.audicaGameState = new AudicaGameStateManager();
-			AudicaHTTPStatus.targetState = new AudicaTargetStateManager();
-
+			AudicaHTTPStatus.AudicaGameState = new AudicaGameStateManager();
+			AudicaHTTPStatus.TargetState = new AudicaTargetStateManager();
 
             this.encoder = new Encoder();
 
 			this.httpServer = new HTTPServer(() => {
 				return this.encoder.Status(
-					AudicaHTTPStatus.audicaGameState.GameState,
-					AudicaHTTPStatus.audicaGameState.SongState
+					AudicaHTTPStatus.AudicaGameState.GameState,
+					AudicaHTTPStatus.AudicaGameState.SongState
 				);
 			});
 			this.httpServer.Initialise();
@@ -75,70 +56,42 @@ namespace AudicaHTTPStatus
 		}
 
 		public override void OnUpdate() {
-			AudicaHTTPStatus.audicaGameState.Update();
-		}
-
-        public static void SelectSong(IntPtr @this) {
-            AudicaHTTPStatus.selectSong.InvokeOriginal(@this);
-            SongSelectItem button = new SongSelectItem(@this);
-            AudicaHTTPStatus.selectedSongData = button.mSongData;
-        }
-
-		public static void StartSong(IntPtr @this) {
-			AudicaHTTPStatus.playSong.InvokeOriginal(@this);
-			AudicaHTTPStatus.audicaGameState.SongStart(AudicaHTTPStatus.selectedSongData);
-		}
-
-		public static void RestartSong(IntPtr @this) {
-			AudicaHTTPStatus.restartSong.InvokeOriginal(@this);
-			AudicaHTTPStatus.audicaGameState.SongRestart();
-		}
-
-		public static void EndSong(IntPtr @this) {
-			AudicaHTTPStatus.endSong.InvokeOriginal(@this);
-			AudicaHTTPStatus.audicaGameState.SongEnd();
-		}
-        
-		/** TARGET EVENT HANDLING **/
-		public unsafe static void TargetHit(IntPtr @this, IntPtr cue, float tick, Vector2 targetHitPos) {
-			AudicaHTTPStatus.targetHit.InvokeOriginal(@this, new IntPtr[] {
-				cue,
-				new IntPtr((void*)(&tick)),
-				new IntPtr((void*)(&targetHitPos))
-			});
-
-			AudicaTargetHitState targetHit = AudicaHTTPStatus.targetState.TargetHit(targetHitPos);
-			// TODO: feed output into JSON parser then to HTTP server as websocket event
-		}
-
-		public unsafe static void TargetMiss(IntPtr @this) {
-			AudicaHTTPStatus.targetMiss.InvokeOriginal(@this);
-
-			AudicaTargetFailState targetMiss = AudicaHTTPStatus.targetState.TargetMiss();
-		}
-
-		public unsafe static void TargetMissAim(IntPtr @this, IntPtr cue, Vector2 targetMissPos) {
-			AudicaHTTPStatus.targetMissAim.InvokeOriginal(@this, new IntPtr[] {
-				cue,
-				new IntPtr((void*)(&targetMissPos))
-			});
-
-			AudicaTargetFailState targetMiss = AudicaHTTPStatus.targetState.TargetMissAim();
-		}
-
-		public unsafe static void TargetMissEarlyLate(IntPtr @this, IntPtr cue, float tick) {
-			AudicaHTTPStatus.targetMissEarlyLate.InvokeOriginal(@this, new IntPtr[] {
-				cue,
-				new IntPtr((void*)(&tick)),
-			});
-
-			AudicaTargetFailState targetMiss = AudicaHTTPStatus.targetState.TargetMissEarlyLate(tick);
-		}
-
-		public unsafe static void Misfire(IntPtr @this) {
-			AudicaHTTPStatus.misfire.InvokeOriginal(@this);
-
-			// TODO (event with hand that misfired?)
+			AudicaHTTPStatus.AudicaGameState.Update();
 		}
 	}
+
+    internal static class Hooks {
+        public static void ApplyHooks(HarmonyInstance instance) {
+            instance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        // song selection + play state
+        [HarmonyPatch(typeof(SongSelectItem), "OnSelect", new Type[0])]
+        public static class SongSelectPatch {
+            public static void Postfix(SongSelectItem __instance) {
+                AudicaHTTPStatus.selectedSongData = __instance.mSongData;
+            }
+        }
+
+        [HarmonyPatch(typeof(ScoreKeeper), "Start", new Type[0])]
+        public static class StartSongPatch {
+            public static void Postfix(ScoreKeeper __instance) {
+                AudicaHTTPStatus.AudicaGameState.SongStart(AudicaHTTPStatus.selectedSongData);
+            }
+        }
+
+        [HarmonyPatch(typeof(InGameUI), "Restart", new Type[0])]
+        public static class RestartSongPatch {
+            public static void Postfix(InGameUI __instance) {
+                AudicaHTTPStatus.AudicaGameState.SongRestart();
+            }
+        }
+
+        [HarmonyPatch(typeof(InGameUI), "ReturnToSongList", new Type[0])]
+        public static class EndSongPatch {
+            public static void Postfix(InGameUI __instance) {
+                AudicaHTTPStatus.AudicaGameState.SongEnd();
+            }
+        }
+    }
 }
